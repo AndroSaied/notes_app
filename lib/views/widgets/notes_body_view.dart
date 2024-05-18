3d832bfd4cb88190f0bee7a46052246b5922a45d
AndroSaied <androsid15@yahoo.com>
2024-05-18 18:57:18 +0300

    fetch notes

--- lib/views/widgets/notes_body_view.dart
+++ lib/views/widgets/notes_body_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
 import 'package:notes_app/views/widgets/notes_list_view.dart';
 
-class NotesViewBody extends StatelessWidget {
+class NotesViewBody extends StatefulWidget {
   const NotesViewBody({super.key});
 
+  @override
+  State<NotesViewBody> createState() => _NotesViewBodyState();
+}
+
+class _NotesViewBodyState extends State<NotesViewBody> {
+
+  @override
+  void initState() {
+    BlocProvider.of<NotesCubit>(context).fetchAllNotes();
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return const Padding(
@@ -16,9 +30,15 @@ class NotesViewBody extends StatelessWidget {
             height: 45,
           ),
       
-          CustomAppBar(title: "Notes", icon: Icons.search,),
+          CustomAppBar(
+            title: "Notes", 
+            icon: Icons.search,
+          ),
 
-          Expanded(child: NotesListView()),
+          Expanded(
+            child: NotesListView(),
+          ),
+          
         ],
       ),
     );
